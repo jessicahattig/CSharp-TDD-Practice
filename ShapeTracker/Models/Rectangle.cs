1b4dfc3015acc329762a73a4b1946f3102b44043
jessicahattig <jessicahattig@gmail.com>
2023-08-27 17:30:21 -0700

    WIP: Rectangle.cs business logic.

---
+++ ShapeTracker/Models/Rectangle.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ShapeTracker.Models
+{
+  public static Rectangle
+  {
+
+    private static List<Rectangle> _instances = new
+  }
+    // {
+    //   _instances.Clear();
+    // }
+}
