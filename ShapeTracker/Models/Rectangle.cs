62f80617a2b04fc0f5958e0df6a4e25af5162276
jessicahattig <jessicahattig@gmail.com>
2023-09-05 19:41:51 -0700

    RectangleTests.cs, test 2, pass: Green phase.

--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -1,14 +1,25 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace ShapeTracker.Models
 {
   public class Rectangle
   {
+    public int Side1 { get; set; }
+    public int Side2 { get; set; }
 
-  //   private static List<Rectangle> _instances = new List<Rectangle> {};
-  // }
-    // {
-    //   _instances.Clear();
-    // }
+    private static List<Rectangle> _instances = new List<Rectangle> {};
+
+    public Rectangle (int length1, int length2)
+    {
+      Side1 = length1;
+      Side2 = length2;
+      _instances.Add(this);
+    }
+
+    public static List<Rectangle> GetAll()
+    {
+      return _instances;
+    }
+    }
   }
-}
